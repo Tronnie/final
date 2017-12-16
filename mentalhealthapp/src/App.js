a86c52b93602658c421dccacaad51305db426643
Stephen Armstrong <starmstrong05@gmail.com>
2017-12-16 13:55:31 -0800

    added blue

--- mentalhealthapp/src/App.js
+++ mentalhealthapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from "./components/Navbar";
+import Activity from "./components/Activity";
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,8 @@ class App extends Component {
     return (
       <div className="App">
       	<Navbar />
-        <h1> APP GOES HERE </h1>
+      	<Activity />
+        
       </div>
     );
   }
---
+++ mentalhealthapp/src/components/Activity/Activity.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Activity.css";
+
+
+class Activity extends React.Component{
+
+
+	//insert funcitons and state stuff here
+
+
+	render() {
+
+
+		return(
+			<div className="container">
+				<div className="row">	
+					<div id="activity" className="col-12 align-self-center">
+						<h1>Welcome to our Mental Health App!!!1!1!</h1>
+					</div>
+				</div>
+			</div>
+
+		)
+	}
+
+}
+
+
+export default Activity;
+
