3b5698dade9231b481880f116af9ab6519bd388a
Prashan Welipitiya <prshn.weli@gmail.com>
2017-12-20 17:53:54 -0800

    add stars

--- mentalhealthapp/src/components/Stars/stars.js
+++ mentalhealthapp/src/components/Stars/stars.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./stars.css";
 
 const Stars = () => (
 
@@ -7,11 +8,11 @@ const Stars = () => (
    
       <ul className="rate-area">
           <hr />
-      <input type="radio" id="5-star" name="rating" value="5" /><label for="5-star" title="Amazing">5 stars</label>
-      <input type="radio" id="4-star" name="rating" value="4" /><label for="4-star" title="Good">4 stars</label>
-      <input type="radio" id="3-star" name="rating" value="3" /><label for="3-star" title="Average">3 stars</label>
-      <input type="radio" id="2-star" name="rating" value="2" /><label for="2-star" title="Not Good">2 stars</label>
-      <input type="radio" id="1-star" name="rating" value="1" /><label for="1-star" title="Bad">1 star</label>
+      <input type="radio" id="1-star" name="rating" disabled value="1" /><label for="1-star" >1</label>
+      <input type="radio" id="2-star" name="rating" disabled value="2" /><label for="2-star" >2</label>
+      <input type="radio" id="3-star" name="rating" disabled value="3" /><label for="3-star" >3</label>
+      <input type="radio" id="4-star" name="rating" disabled value="4" /><label for="4-star" >4</label>
+      <input type="radio" id="5-star" name="rating" disabled value="5" /><label for="5-star" >5</label>
       
      </ul>
     
