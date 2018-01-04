daeb77649c4040b5067e71ce015d87694e06273b
Lee <pflee@nala.roche.com>
2018-01-03 19:33:30 -0800

    Login.js edited, Register component created

--- mentalhealthapp/src/components/Login/Login.js
+++ mentalhealthapp/src/components/Login/Login.js
@@ -1,43 +1,80 @@
-import React from "react";
-import "./Login.css";
+// import React from "react";
+// import "./Login.css";
 
 
-class Login extends React.Component {
+// class Login extends React.Component {
 
 
 
-	sayHello(userName) {
+// 	sayHello(userName) {
 
-		console.log(userName)
+// 		console.log(userName)
 
-	}
+// 	}
 
 	
 
 
 
-	render() {
+// 	render() {
 
 
-		return(
+// 		return(
 
-			<div>
+// 			<div>
 				
-				<div className="login">
-					<label className="modal"><b>Username</b></label>
-   					<input  type="text" placeholder="Enter Username" name="uname" require/>
-					<label className="modal"><b>Password</b></label>
-				    <input   type="password" placeholder="Enter Password" name="psw" required/>
+// 				<div className="login">
+// 					<label className="modal"><b>Username</b></label>
+//    					<input  type="text" placeholder="Enter Username" name="uname" require/>
+// 					<label className="modal"><b>Password</b></label>
+// 				    <input   type="password" placeholder="Enter Password" name="psw" required/>
 
-				    <button type="submit">Login</button>
-				    <input type="checkbox" checked="checked"/>
-				</div>
+// 				    <button type="submit">Login</button>
+// 				    <input type="checkbox" checked="checked"/>
+// 				</div>
+// 			</div>
+
+// 			)
+// 		}
+// 	}
+
+
+
+import React from "react"
+import "./Login.css";
+
+class Login extends React.Component {
+	// refs
+	form: null;
+	usernameElem: null;
+	passwordElem: null;
+
+	render() {
+		const { onLogin } = this.props;
+		return (
+			<div className="container">
+				<form
+					ref={(elem) => this.form = elem}
+					onSubmit={(e) => {
+						e.preventDefault();
+						return onLogin({
+							username: this.usernameElem.value,
+							password: this.passwordElem.value
+						});
+					}}
+				>
+					<input ref={(input) => this.usernameElem = input} type='text' name="username" placeholder='Enter Username' />
+					<input ref={(input) => this.passwordElem = input} type='password' name="password" placeholder='Password' />
+					<button
+						className="btn btn-default"
+						type='submit'
+					>
+						Submit
+					</button>
+				</form>
 			</div>
-
-			)
-		}
+		)
 	}
+}
 
-
-
-export default Login;
+export default Login
